63079e842adccb10b6885c5997db230c8d0929b0
crangeltx <cbarrontx@yahoo.com>
2021-07-16 21:37:23 -0500

    added jquery

---
+++ Js/index.js
@@ -0,0 +1,72 @@
+// .home hover & cursor change//
+$(".home").mouseover(function() {
+    $(this).css("color", "#00D1FF");
+}).mouseleave (function() {
+    $(this).css("color", "#ffffff");
+});
+
+$(".about_me").hover(function() {
+    $(this).css("cursor","pointer");
+}, function() {
+    $(this).css("cursor", "auto");
+});
+
+
+// .about_me hover & cursor change//
+$(".about_me").mouseover(function() {
+    $(this).css("color", "#00D1FF");
+}).mouseleave (function() {
+    $(this).css("color", "#ffffff");
+});
+
+$(".aboutMe").hover(function() {
+    $(this).css("cursor","pointer");
+}, function() {
+    $(this).css("cursor", "auto");
+});
+
+
+// .contact hover & cursor change//
+$(".contact").mouseover(function() {
+    $(this).css("color", "#00D1FF");
+}).mouseleave (function() {
+    $(this).css("color", "#ffffff");
+});
+
+$(".contact").hover(function() {
+    $(this).css("cursor","pointer");
+}, function() {
+    $(this).css("cursor", "auto");
+});
+
+
+// .projects hover & cursor change//
+$(".projects").mouseover(function() {
+    $(this).css("color", "#00D1FF");
+}).mouseleave (function() {
+    $(this).css("color", "#ffffff");
+});
+
+$(".projects").hover(function() {
+    $(this).css("cursor","pointer");
+}, function() {
+    $(this).css("cursor", "auto");
+});
+
+
+
+// .linkedin hover & cursor change//
+$(".linkedin").mouseover(function() {
+    $(this).css("color", "#00D1FF");
+}).mouseleave (function() {
+    $(this).css("color", "#ffffff");
+});
+
+$(".linkedin").hover(function() {
+    $(this).css("cursor","pointer");
+}, function() {
+    $(this).css("cursor", "auto");
+});
+
+
+
